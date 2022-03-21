1ada811fe34df870ec75493f5e61267b725d8580
Nick Dubelman <ndubelman@gmail.com>
2022-03-21 15:43:02 -0700

    TestSelectAll

--- select_test.go
+++ select_test.go
@@ -1,9 +1,49 @@
 package azamat
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestSelectAll(t *testing.T) {
-	t.Error("epic failure")
+	db, _ := sqlx.Open("sqlite3", ":memory:")
+
+	type Todo struct {
+		ID    int
+		Title string
+	}
+
+	db.MustExec(`CREATE TABLE todos (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT NOT NULL
+	)`)
+
+	// When there are no entries in the table...
+	query := Select[Todo]("id", "title").From("todos")
+
+	todos, err := query.All(db)
+	require.NoError(t, err)
+	require.Zero(t, todos)
+
+	// Create some todos
+	todo1, todo2 := "assist Borat", "find Pamela"
+	db.MustExec(`INSERT INTO todos (title) VALUES (?), (?)`, todo1, todo2)
+
+	// When there are multiple entries in the table...
+	todos, err = query.All(db)
+	require.NoError(t, err)
+	require.Len(t, todos, 2)
+
+	// When not just fetching all...
+	query = query.Where("id = ?", 2)
+	todos, err = query.All(db)
+	require.NoError(t, err)
+	require.Len(t, todos, 1)
+	assert.Equal(t, todo2, todos[0].Title)
 }
 
 func TestSelectOnly(t *testing.T) {
