10855972a869a8f7de8f08656d4e36366d8b11e4
Nick Dubelman <ndubelman@gmail.com>
2022-03-21 15:48:44 -0700

    TestSelectOnly

--- select_test.go
+++ select_test.go
@@ -47,5 +47,35 @@ func TestSelectAll(t *testing.T) {
 }
 
 func TestSelectOnly(t *testing.T) {
-	t.Error("epic failure")
+	db, _ := sqlx.Open("sqlite3", ":memory:")
+
+	type Todo struct {
+		ID    int
+		Title string
+	}
+
+	db.MustExec(`CREATE TABLE todos (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT NOT NULL
+	)`)
+
+	// When there are no entries in the table...
+	query := Select[Todo]("id", "title").From("todos")
+
+	todo, err := query.Only(db)
+	require.Error(t, err)
+
+	// Create some todos
+	todo1, todo2 := "assist Borat", "find Pamela"
+	db.MustExec(`INSERT INTO todos (title) VALUES (?), (?)`, todo1, todo2)
+
+	// When there are multiple entries in the result...
+	todo, err = query.Only(db)
+	require.Error(t, err)
+
+	// When there is only a single entry in the result...
+	query = query.Where("id = ?", 2)
+	todo, err = query.Only(db)
+	require.NoError(t, err)
+	assert.Equal(t, todo2, todo.Title)
 }
