9c62dc5fe9688323e8ec87d85d807ebf6cbbd832
Nick Dubelman <ndubelman@gmail.com>
2022-03-25 15:50:08 -0700

    TestViewGetByID

--- view_test.go
+++ view_test.go
@@ -95,7 +95,83 @@ func TestViewGetAll(t *testing.T) {
 }
 
 func TestViewGetByID(t *testing.T) {
-	t.Error("epic failure")
+	db, _ := sqlx.Open("sqlite3", ":memory:")
+
+	type User struct {
+		ID   int
+		Name string
+	}
+
+	type Todo struct {
+		ID     int
+		Title  string
+		Author string
+	}
+
+	type TodoRow struct {
+		ID       int
+		Title    string
+		AuthorID int `db:"authorID"`
+	}
+
+	UserTable := Table[User]{
+		Name:    "users",
+		Columns: []string{"id", "name"},
+	}
+
+	TodoTable := Table[TodoRow]{
+		Name:    "todos",
+		Columns: []string{"id", "title", "authorID"},
+	}
+
+	TodoView := View[Todo]{
+		IDFrom: TodoTable,
+		Query: func() sq.SelectBuilder {
+			join := fmt.Sprintf(
+				"%s ON %s.id = %s.authorID", UserTable, UserTable, TodoTable,
+			)
+
+			return TodoTable.
+				SelectColumns("id", "title").
+				Columns("name AS author").
+				Join(join)
+		},
+	}
+
+	db.MustExec(`CREATE TABLE users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL
+	)`)
+
+	db.MustExec(`CREATE TABLE todos (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT NOT NULL,
+		authorID INTEGER NOT NULL,
+		
+		FOREIGN KEY (authorID) REFERENCES users(id)
+	)`)
+
+	// When there are no entries in the table...
+	todo, err := TodoView.GetByID(db, 420)
+	require.Error(t, err)
+	require.Zero(t, todo)
+
+	// Create some users
+	user1, user2 := "Azamat", "Borat"
+	db.MustExec(`INSERT INTO users (name) VALUES (?), (?)`, user1, user2)
+
+	// Create some todos
+	todo1, todo2 := "assist Borat", "find Pamela"
+	db.MustExec(
+		`INSERT INTO todos (title, authorID) VALUES (?, 2), (?, 1)`, todo1, todo2,
+	)
+
+	// When there are multiple entries in the table...
+	todo, err = TodoView.GetByID(db, 2)
+	require.NoError(t, err)
+	assert.Equal(t, 2, todo.ID)
+	assert.Equal(t, todo2, todo.Title)
+	assert.Equal(t, user1, todo.Author)
 }
 
 func TestViewGetByIDs(t *testing.T) {
